87643108beb140004dacb910295ffa08bb2b1975
N721 <53975284+N721@users.noreply.github.com>
2020-05-28 05:27:03 +0700

    Create up.php

---
+++ up.php
@@ -0,0 +1,9 @@
+<center>
+<form method='POST' enctype='multipart/form-data'>
+<b>UPLOAD FILE</b><p>
+<input type='file' name='uploads' size='30' style='font-size: 10pt; color: #008000; font-family: Tahoma; border: 1px inset #C0C0C0; background-color: #FFFFFF; font-weight:bold'>
+<br>
+<input type='submit' value='Upload' size='50' style='font-size: 8pt; color: #000080; font-family: Tahoma; border: 1px dashed #FFFFFF; background-color: #FFFFFF; font-weight:bold'>
+</p>
+</form></center></td></tr>
+</table><br>
